f5b3fdaf1fadbbaa84abe84baef17729daf5e78f
Orion Cable <noiro.elbac@gmail.com>
2024-03-21 13:49:09 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import { useState } from "react";
+import { useZxing } from "react-zxing";
+
+function App () {
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
+
+  return (
+    <>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
+    </>
+  );
+};
+
+export default App
+
+
+
